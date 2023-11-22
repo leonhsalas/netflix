c8c3910640e867186fd5355ca5a94823b8c52c3f
Leon Nicolhas <leonnhsalas@gmail.com>
2023-11-22 15:33:03 -0600

    upgrade

--- main.js
+++ main.js
@@ -5,20 +5,22 @@ const play = document.getElementById("primero");
 
 const mylist = document.getElementById("segundo");
 
+const vid = document.getElementById("vid")
+
 let bin = false;
 
 function red(){
    if(bin == false){
         bin = !bin;
-        play.toggle("transparent")
-        mylist.toggle("red")
+        play.style.backgroundColor = 'transparent';
+        mylist.style.backgroundColor = 'red';
     }else{
         bin = !bin;
-        play.remove("transparent")
-        mylist.remove("red")
+        play.style.backgroundColor = 'red'
+        mylist.style.backgroundColor = 'transparent'
     }
 }
 
 function ope(){
-    
+    vid.style.display = "block"
 }
